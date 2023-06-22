ff774c19614fd8c7cb172f15d789f19e072699bc
mokhelek <mkatleho191@gmail.com>
2023-06-22 17:06:48 +0200

    add empty input validation

--- src/App.jsx
+++ src/App.jsx
@@ -12,36 +12,70 @@ function App() {
 
     useEffect(() => {
         setRegNumList(storedRegNums);
-        
     }, []);
 
-    const [filtered, setFiltered] = useState(storedRegNums)
-
+    const [filtered, setFiltered] = useState(storedRegNums);
 
     function handleSubmit(regNum) {
-        const newReg = [regNum, ...regNumList];
-        setRegNumList(newReg);
-        setFiltered(newReg)
-        localStorage.setItem("items", JSON.stringify(newReg));
+        let duplicateError = document.querySelector("#duplicate-error");
+        let emptyInputError = document.querySelector("#empty-input-error");
+        let formatError = document.querySelector("#format-error");
+
+        if (regNum != "") {
+            const newReg = [regNum, ...regNumList];
+            setRegNumList(newReg);
+            setFiltered(newReg);
+            localStorage.setItem("items", JSON.stringify(newReg));
+            // if (regFactory.regFormatCheck(regInput.value.replace(/\s/g, ""))) {
+            //     if (regFactory.handleDuplicates(regInput.value.toUpperCase().replace(/\s/g, ""))) {
+            //         const listItem = document.createElement("li");
+
+            //         regFactory.addRegNum(regInput.value.toUpperCase().replace(/\s/g, ""));
+
+            //         listItem.textContent = registrationNumbers[registrationNumbers.length - 1];
+            //         regNumList.appendChild(listItem);
+
+            //         regNumList.insertBefore(listItem, regNumList.childNodes[0]);
+
+            //         localStorage["regNumbersList"] = regFactory.getRegistrations();
+
+            //         townCounters();
+            //     } else {
+            //         duplicateError.style.display = "block";
+            //         setTimeout(function () {
+            //             duplicateError.style.display = "none";
+            //         }, 3000);
+            //     }
+            // } else {
+            //     formatError.style.display = "block";
+            //     setTimeout(function () {
+            //         formatError.style.display = "none";
+            //     }, 3000);
+            // }
+        } else {
+            emptyInputError.style.display = "block";
+            setTimeout(function () {
+                emptyInputError.style.display = "none";
+            }, 3000);
+        }
     }
 
     function filterRegNumbers(town) {
         let filteredArray = [];
-        if(town == "ALL"){
-            filteredArray = [...regNumList] ;
-        }else{
+        if (town == "ALL") {
+            filteredArray = [...regNumList];
+        } else {
             regNumList.filter(function (item) {
-                let currentItem = item.toUpperCase()
+                let currentItem = item.toUpperCase();
                 if (currentItem.startsWith(town)) {
-                    filteredArray.push(item) ;
+                    filteredArray.push(item);
                 }
             });
         }
-        setFiltered(filteredArray)
+        setFiltered(filteredArray);
         // return filteredArray
     }
 
-    
     return (
         <div style={{ marginTop: "3rem" }} className="container">
             <Header onSubmit={handleSubmit} />
