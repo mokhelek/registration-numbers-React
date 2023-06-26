2d3f1b7226f17126aab2b99017038964f7dad438
mokhelek <mkatleho191@gmail.com>
2023-06-27 00:41:47 +0200

    fixed bug with image file path

--- src/components/EmptyList.jsx
+++ src/components/EmptyList.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import myImage from '../assets/undraw_void_-3-ggu.svg'
 
 function EmptyList() {
   return (
     <div>
         <p style={{fontSize:"1.3rem"}} > ~ No registration numbers  ~ </p>
-        <img style={{width:"50%"}} className='img-fluid' src={"src/assets/undraw_void_-3-ggu.svg"} alt="" />
+        <img style={{width:"50%"}} className='img-fluid' src={myImage} alt="" />
     </div>
   )
 }
