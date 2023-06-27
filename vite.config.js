9bcdf21d4774c5ff394bede88428d38b1ff15890
mokhelek <mkatleho191@gmail.com>
2023-06-27 16:44:49 +0200

    more configurations

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/registration-numbers',
 })
 
-if (command !== 'serve') {
-  config.base = '/registration-numbers/'
-}
+// if (command !== 'serve') {
+//   config.base = '/registration-numbers/'
+// }
 
-return config ;
+// return config ;
